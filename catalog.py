ad11ba46281c57f8f3e3556dcd1282459fcc32fc
Hayden Oster <hayden94@gmail.com>
2017-03-20 20:45:11 -0700

    Google Connect(w/ supporting routes) and html/css updates

--- catalog.py
+++ catalog.py
@@ -11,8 +11,12 @@ import json
 from flask import make_response
 import requests
 
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Restaurant Menu App"
+
 # import CRUD operations
-from db_setup import Base, Category, Item
+from db_setup import Base, Category, Item, User
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 
@@ -22,7 +26,7 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
-#JSON routes
+#JSON endpoint routes
 @app.route('/categories/JSON')
 def categoriesJSON():
     categories = session.query(Category).all()
@@ -34,10 +38,164 @@ def itemsJSON(category_id):
     items = session.query(Item).filter_by(category_id=category_id).all()
     return jsonify(Items=[i.serialize for i in items])
 
+# Authentication/authorization routes
 @app.route('/login')
 def showLogin():
     return render_template('login.html')
 
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope="")
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+                                 json.dumps('Failed to upgrade the authorization code.'), 401
+                                 )
+        response.headers['Content-type'] = 'application/json'
+        print "Failed to upgrade the authorization code."
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+
+    # If there was an error in the access token info, abort.
+    if result.get('error') in not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        print "error"
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+                                 json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        print "Token's user ID doesn't match given user ID."
+        return response
+
+    # verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+                                json.dumps("Token's client ID does not match the app's."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        print "Token's client ID does not match app's"
+        return response
+
+    stored_credentials = login_session.get('credentials')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_credentials is not None and gplus_id == stored_gplus_id:
+        response = make_response(
+                                 json.dumps('Current user is already connected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        print "Current user is already connected."
+        return response
+
+    # Store the access token in the session for later use.
+    loggin_session['credentials'] = credentials
+    loggin_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    loggin_session['username'] = data['name']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += "<div style='text-align: center; position: relative; margin: 0 auto; display: block;'>"
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += "</div>"
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+# User helper models
+def createUser(login_session):
+    newUser = User(
+                        name = login_session['username'],
+                        email = login_session['email'],
+                        picture=login_session['picture']
+                        )
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+# Gaccount - revoke a current user's token and reset their login_session
+@app.route('/gdisconnect')
+def gdisconnect():
+    credentials = login_session.get('credentials')
+    access_token = credentials.access_token
+    print 'In gdisconnect access token is %s' % access_token
+    print 'User name is: '
+    print login_session['username']
+
+    if access_token is None:
+        response = make_response(json.dumps('Current user not connected.'), 401)
+        response.header['Content-Type'] = 'application/json'
+        print "Current user not connected."
+        return response
+
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+            % access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    print 'result is '
+    print result
+
+    if result['status'] == '200':
+        del login_session['credentials']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+
+        response = make_response(json.dumps('Successfully Disconnected.'), 200)
+        response.headers['Content-Type'] = 'application/json'
+        print "Successfully Disconnected."
+        return response
+    else:
+        response = make_response(json.dumps('Failed to revoke token for given user.'), 400)
+        response.headers['Content-Type'] = 'application/json'
+        print "Failed to revoke token for given user."
+        return response
+
+# Category Routes
 @app.route('/')
 @app.route('/categories')
 def showMain():
@@ -45,7 +203,6 @@ def showMain():
 
     return render_template('main.html', categories=categories)
 
-# Category routes
 @app.route('/categories/new', methods=['GET', 'POST'])
 def newCategory():
     if request.method == 'POST':
@@ -100,6 +257,12 @@ def deleteCategory(category_id):
         return render_template('deleteCategory.html', category_id=category_id, category=deleteCategory)
 
 # Item routes
+@app.route('/categories/<int:category_id>/<int:item_id>/')
+def showItem(category_id, item_id):
+    item = session.query(Item).filter_by(id=item_id).one()
+
+    return render_template('showItem.html', category_id=category_id, item_id=item_id, item=item)
+
 @app.route('/categories/<int:category_id>/new', methods=['GET', 'POST'])
 def newItem(category_id):
     if request.method == 'POST':
@@ -120,12 +283,6 @@ def newItem(category_id):
             return render_template('newItem.html', category_id=category_id)
     else:
         return render_template('newItem.html', category_id=category_id)
-
-@app.route('/categories/<int:category_id>/<int:item_id>/')
-def showItem(category_id, item_id):
-    item = session.query(Item).filter_by(id=item_id).one()
-
-    return render_template('showItem.html', category_id=category_id, item_id=item_id, item=item)
 
 @app.route('/categories/<int:category_id>/<int:item_id>/edit', methods=['GET', 'POST'])
 def editItem(category_id, item_id):
@@ -160,8 +317,6 @@ def deleteItem(category_id, item_id):
 
     else:
         return render_template('deleteItem.html', category_id=category_id, item_id=item_id, item=deleteItem)
-
-
 
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
--- db_setup.py
+++ db_setup.py
@@ -8,17 +8,28 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 class Category(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(120), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
         return {
             'name': self.name,
-            'id': self.id
+            'id': self.id,
+            'user_id': self.user_id
         }
 
 class Item(Base):
@@ -29,13 +40,16 @@ class Item(Base):
     description = Column(String(500))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
     @property
     def serialize(self):
         return {
             'name': self.name,
             'id': self.id,
-            'description': self.description
+            'description': self.description,
+            'user_id': self.user_id
         }
 
 engine = create_engine('sqlite:///catalog.db')
